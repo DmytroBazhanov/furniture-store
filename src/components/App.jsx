2780cb8f398ec9b73cd4f69606827e84f158a5e4
Dmytro Bazhanov <dimafg12@gmail.com>
2023-03-29 19:08:14 +0300

    Firebase - Connect firebase
    Firebase - Firebase was installed and configured
    Firebase - A temporary button added to App.jsx to test firebase requests
    Firebase - 2 get requests created to read products

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { getProductsWithPagination } from "../queries/products";
+import { useState } from "react";
 
 function App() {
+    const [lastProd, setLP] = useState(null);
+
+    const test = async () => {
+        const result = await getProductsWithPagination(lastProd, 2);
+        console.log(result);
+
+        setLP(result.lastProductFirebaseSnapshot);
+    };
+
     return (
         <div className="App">
             <h1>Hey! I'm App.jsx!</h1>
+            <button onClick={test}>Get product 9HwUtcBFohVDKM0mywtm</button>
             <Outlet />
         </div>
     );
---
+++ src/queries/products.js
@@ -0,0 +1,50 @@
+import { db } from "../firebase";
+import { collection, query, limit, doc, getDoc, getDocs, startAfter } from "firebase/firestore";
+
+export async function getProductByID(id) {
+    const docRef = doc(db, import.meta.env.VITE_PRODUCT_COLLECTION, id);
+    const product = await getDoc(docRef);
+
+    if (product.exists()) {
+        const data = product.data();
+        return data;
+    }
+
+    throw new Error("Product doesnt exist", `No product found with id - ${id}`);
+}
+
+export async function getProductsWithPagination(lastProduct, productLimit) {
+    const collectionRef = collection(db, import.meta.env.VITE_PRODUCT_COLLECTION);
+    let collectionQuery = null;
+
+    if (lastProduct === null) {
+        collectionQuery = query(collectionRef, limit(productLimit));
+    } else {
+        collectionQuery = query(collectionRef, limit(productLimit), startAfter(lastProduct));
+    }
+
+    const productsCollection = await getDocs(collectionQuery);
+
+    if (productsCollection.docs.length === 0 && lastProduct === null) {
+        throw new Error(
+            "Empty collection",
+            "Response received but the collection is empty, it is possible that collection name was specified wrongly"
+        );
+    }
+
+    const products = [];
+
+    productsCollection.forEach((prod) => {
+        products.push({
+            id: prod.id,
+            ...prod.data(),
+        });
+    });
+
+    const lastDoc = productsCollection.docs[productsCollection.docs.length - 1];
+
+    return {
+        products,
+        lastProductFirebaseSnapshot: lastDoc,
+    };
+}
