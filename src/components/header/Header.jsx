9b4a550d2ada5c1081ab63d52ac32323679c98c8
Dmytro Bazhanov <dimafg12@gmail.com>
2023-04-10 17:06:09 +0300

    HEADER LAYOUTS - Add mobile + tablet + laptop layouts to header
    HEADER LAYOUTS - layouts added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
-import "./header.scss";
+import { ReactComponent as Hamburger } from "../../assets/Hamburger.svg";
 import SaleBadge from "../saleBadge/SaleBadge";
-import DropdownMenu from "../dropdownMenu/DropdownMenu";
 import Cart from "../cart/Cart";
 import Notifications from "../Notifications/Notifications";
 import LoginArea from "../loginArea/LoginArea";
 
+import "./header.scss";
+
 export default function Header() {
+    const salesBadgeMobile = <SaleBadge />; // for mobile layout
+
     return (
         <header>
+            <Hamburger className="hamburger" />
             <h1 className="logo">My-Furniture</h1>
             <nav>
                 <Link to="/categories">Categories</Link>
                 <Link to="/categories/bestSellers">Best Sellers</Link>
                 <Link to="/FAQ">FAQs</Link>
             </nav>
+            {salesBadgeMobile}
             <div className="header-rightSide">
                 <SaleBadge />
                 <Cart />
