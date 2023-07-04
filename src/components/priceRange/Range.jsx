e33336dc3e4ac8cc9ede06dfc941a1263f41b2ce
Dmytro Bazhanov <dimafg12@gmail.com>
2023-07-04 19:18:06 +0300

    PLP price range - create price range
    PLP price range - design of price range changed
    PLP price range - max price range created

--- src/components/priceRange/Range.jsx
+++ src/components/priceRange/Range.jsx
@@ -1,94 +1,59 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getEdgePrices } from "../../queries/filters";
-import { CONTROLLER_PADDING_FROM_RANGE_START, RANGE_CONTROLLER_SIZE } from "./config";
 
-export default function Range({ setMin, setMax }) {
-    const [firstRangeValue, setFirstRangeValue] = useState(0);
-    const [secondRangeValue, setSecondRangeValue] = useState(0);
-    const [fractalPrice, setFracPrice] = useState(0);
+export default function Range({ minPrice, maxPrice, setMin, setMax }) {
     const [minPossibleValue, setMinPossible] = useState(0);
     const [maxPossibleValue, setMaxPossible] = useState(0);
 
-    const rangeRef = useRef(null);
+    const tailRef = useRef(null);
 
-    const getPrices = async () => {
-        const edgePrices = await getEdgePrices("kitchen");
-
-        const min = edgePrices[0];
-        const max = edgePrices[1];
-
-        setMin(min);
-        setMax(max);
-        setMinPossible(min);
-        setMaxPossible(max);
-
-        const fractalCount = rangeRef.current.offsetWidth - RANGE_CONTROLLER_SIZE * 2;
-        setFracPrice((max - min) / fractalCount);
+    const handleMinChange = (event) => {
+        const val = Number(event.target.value);
+        if (maxPrice >= val) setMin(val);
+        else setMin(maxPrice);
     };
 
-    const moveRight = (event) => {
-        const elemRect = rangeRef.current.getBoundingClientRect();
-    };
-
-    const startRightDrag = () => {
-        document.addEventListener("mousemove", moveRight);
-        document.addEventListener("mouseup", endRightDrag);
-        document.body.style.userSelect = "none";
-    };
-
-    const endRightDrag = () => {
-        document.removeEventListener("mousemove", moveRight);
-        document.removeEventListener("mouseup", endRightDrag);
-        document.body.style.userSelect = "auto";
-    };
-
-    const moveLeft = (event) => {
-        const elemRect = rangeRef.current.getBoundingClientRect();
-        // console.log(event.pageX);
-        console.log(elemRect.left);
-        if (event.pageX <= elemRect.left + 11) {
-            setFirstRangeValue(0);
-            setMin(minPossibleValue);
-        } else if (event.pageX >= secondRangeValue + 10) {
-            setFirstRangeValue(secondRangeValue - 20);
-            setMin(maxPossibleValue);
-        } else {
-            setFirstRangeValue(event.pageX - elemRect.left - 10);
-            setMin((event.pageX - RANGE_CONTROLLER_SIZE) * fractalPrice);
-        }
-    };
-
-    const startLeftDrag = () => {
-        document.addEventListener("mousemove", moveLeft);
-        document.addEventListener("mouseup", endLeftDrag);
-        document.body.style.userSelect = "none";
-    };
-
-    const endLeftDrag = () => {
-        document.removeEventListener("mousemove", moveLeft);
-        document.removeEventListener("mouseup", endLeftDrag);
-        document.body.style.userSelect = "auto";
+    const handleMaxChange = (event) => {
+        const val = Number(event.target.value);
+        if (minPrice <= val) setMax(val);
+        else setMax(minPrice);
+        // 200 = width
+        // tailRef.current.style.width = `${(200 / 100) * ((val / maxPossibleValue) * 100) - 1}px`; // for min
+        tailRef.current.style.width = `${200 - (200 / 100) * ((val / maxPossibleValue) * 100)}px`; // for max
     };
 
     useEffect(() => {
-        getPrices();
-        setSecondRangeValue(rangeRef.current.offsetWidth - RANGE_CONTROLLER_SIZE);
+        getEdgePrices("kitchen").then((values) => {
+            setMinPossible(values[0]);
+            setMaxPossible(values[1]);
+            setMin(values[0]);
+            setMax(values[1]);
+        });
     }, []);
 
     return (
-        <div className="range" ref={rangeRef}>
-            <div
-                className="firstEdge"
-                id="firstEdge"
-                style={{ left: firstRangeValue }}
-                onMouseDown={startLeftDrag}
-            ></div>
-            <div
-                className="secondEdge"
-                id="secondEdge"
-                style={{ left: secondRangeValue }}
-                onMouseDown={startRightDrag}
-            ></div>
+        <div className="rangeContainer">
+            <div className="inputContainer">
+                <input
+                    className="maxEdge"
+                    type="range"
+                    min={minPossibleValue}
+                    max={maxPossibleValue}
+                    value={maxPrice}
+                    step={0.01}
+                    onChange={handleMaxChange}
+                />
+                <div className="tail" ref={tailRef}></div>
+            </div>
+            <input
+                className="minEdge"
+                type="range"
+                min={minPossibleValue}
+                max={maxPossibleValue}
+                value={minPrice}
+                step={0.01}
+                onChange={handleMinChange}
+            />
         </div>
     );
 }
