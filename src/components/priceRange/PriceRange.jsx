b746edf17b58bbfe7e55cb0bce8c31fea2937efe
Dmytro Bazhanov <dimafg12@gmail.com>
2023-07-05 02:59:46 +0300

    PLP price range - create a price range
    PLP price range - range added

--- src/components/priceRange/PriceRange.jsx
+++ src/components/priceRange/PriceRange.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import Range from "./Range";
 import "./priceRange.scss";
 
-export default function PriceRange() {
+export default function PriceRange({ width = 200 }) {
     const [minPrice, setMin] = useState(0);
     const [maxPrice, setMax] = useState(0);
 
     return (
         <div className="priceRangeContainer">
             <h1 className="priceRangeHeader filterGroupName">Price range</h1>
-            <Range minPrice={minPrice} maxPrice={maxPrice} setMax={setMax} setMin={setMin} />
+            <Range
+                minPrice={minPrice}
+                maxPrice={maxPrice}
+                setMax={setMax}
+                setMin={setMin}
+                width={width}
+            />
             <br />
             {minPrice}
             ||
--- src/components/priceRange/Range.jsx
+++ src/components/priceRange/Range.jsx
@@ -1,25 +1,30 @@
 import { useState, useEffect, useRef } from "react";
 import { getEdgePrices } from "../../queries/filters";
 
-export default function Range({ minPrice, maxPrice, setMin, setMax }) {
+export default function Range({ minPrice, maxPrice, setMin, setMax, width }) {
     const [minPossibleValue, setMinPossible] = useState(0);
     const [maxPossibleValue, setMaxPossible] = useState(0);
 
     const tailRef = useRef(null);
+    const secondTailRef = useRef(null);
 
     const handleMinChange = (event) => {
         const val = Number(event.target.value);
         if (maxPrice >= val) setMin(val);
         else setMin(maxPrice);
+        secondTailRef.current.style.width = `${
+            (200 / 100) * ((val / maxPossibleValue) * 100) - 1
+        }px`;
     };
 
     const handleMaxChange = (event) => {
         const val = Number(event.target.value);
         if (minPrice <= val) setMax(val);
         else setMax(minPrice);
-        // 200 = width
         // tailRef.current.style.width = `${(200 / 100) * ((val / maxPossibleValue) * 100) - 1}px`; // for min
-        tailRef.current.style.width = `${200 - (200 / 100) * ((val / maxPossibleValue) * 100)}px`; // for max
+        tailRef.current.style.width = `${
+            width - (width / 100) * ((val / maxPossibleValue) * 100)
+        }px`; // for max
     };
 
     useEffect(() => {
@@ -28,6 +33,7 @@ export default function Range({ minPrice, maxPrice, setMin, setMax }) {
             setMaxPossible(values[1]);
             setMin(values[0]);
             setMax(values[1]);
+            tailRef.current.style.width = `0px`;
         });
     }, []);
 
@@ -43,17 +49,21 @@ export default function Range({ minPrice, maxPrice, setMin, setMax }) {
                     step={0.01}
                     onChange={handleMaxChange}
                 />
-                <div className="tail" ref={tailRef}></div>
+                <div className="tail" id="tail1" ref={tailRef}></div>
             </div>
-            <input
-                className="minEdge"
-                type="range"
-                min={minPossibleValue}
-                max={maxPossibleValue}
-                value={minPrice}
-                step={0.01}
-                onChange={handleMinChange}
-            />
+            <div className="sliderPath" style={{ width: width }}></div>
+            <div className="inputContainer">
+                <input
+                    className="minEdge"
+                    type="range"
+                    min={minPossibleValue}
+                    max={maxPossibleValue}
+                    value={minPrice}
+                    step={0.01}
+                    onChange={handleMinChange}
+                />
+                <div className="tail" id="tail2" ref={secondTailRef}></div>
+            </div>
         </div>
     );
 }
