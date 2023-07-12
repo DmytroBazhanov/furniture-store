c4dccc7a23213d20b47e50cec6da820bdb742b0e
Dmytro Bazhanov <dimafg12@gmail.com>
2023-07-12 18:49:43 +0300

    Lagging sidebar - Fix sidebars lagging animation on opening
    Lagging sidebar - backdrop-filter(blur) now applies after animation
    Lagging sidebar - lags disappeared

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from "react";
 import { ReactComponent as Cross } from "../../assets/cross.svg";
+import { DEFAULT_ANIMATION_TIMING } from "./config.js";
 
 import "./sidebar.scss";
 
 export default function Sidebar({ headerContent, mainContent, setVisibility, visible }) {
+    // Blur in combination with element transition cause lags
+    const [isBackgroundActive, setBackground] = useState(false);
+    const [timerID, setTimerID] = useState(null);
+
+    // Setting it after the main animation ended boost performance and removes lagging animation
+    const handleBackgroundChange = () => {
+        const timer = setTimeout(() => {
+            setBackground(true);
+        }, DEFAULT_ANIMATION_TIMING);
+
+        setTimerID(timer);
+    };
+
+    useEffect(() => {
+        if (visible) {
+            handleBackgroundChange();
+        } else if (!visible) {
+            clearTimeout(timerID);
+            setBackground(false);
+        }
+
+        return () => {
+            clearTimeout(timerID);
+        };
+    }, [visible]);
+
     const visibilityClass = visible ? "visible" : "hidden";
+    const backgroundClass = isBackgroundActive ? visibilityClass : "hidden";
 
     return (
-        <div className={`sidebar-background ${visibilityClass}`}>
+        <div className={`sidebar-background ${backgroundClass}`}>
             <aside className={`sidebar ${visibilityClass}`}>
                 <div className="sidebar-topPart">
                     {headerContent}
---
+++ src/components/sidebar/config.js
@@ -0,0 +1,1 @@
+export const DEFAULT_ANIMATION_TIMING = 200; //ms
