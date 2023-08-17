a7d99f22b009b1ded0d84da5c8c4f3e9b98e6da9
Dmytro Bazhanov <dimafg12@gmail.com>
2023-08-18 02:33:11 +0300

    Service Worker - Create service worker
    Service Worker - CDN links removed from index.html
    Service Worker - fonts are stored locally now
    Service Worker - Client caching added to application

---
+++ serviceWorkerFunctionality.js
@@ -0,0 +1,64 @@
+const cacheName = "v1";
+
+const keyResources = ["/", "/index.html"];
+
+self.addEventListener("install", (ev) => {
+    ev.waitUntil(
+        caches.delete(cacheName).then(() => {
+            caches.open(cacheName).then((caches) => {
+                return caches.addAll(keyResources);
+            });
+        })
+    );
+});
+
+self.addEventListener("activate", (ev) => {
+    ev.waitUntil(
+        caches.keys().then((cacheKeys) => {
+            cacheKeys.forEach((cacheKey) => {
+                if (cacheKey !== cacheName) caches.delete(cacheKey);
+            });
+        })
+    );
+});
+
+self.addEventListener("fetch", (ev) => {
+    if (ev.request.mode === "navigate") {
+        return ev.respondWith(
+            caches.match("/index.html").then((cacheResponse) => cacheResponse || fetch(ev.request))
+        );
+    }
+
+    return ev.respondWith(
+        caches.match(ev.request).then((cacheResponse) => {
+            if (ev.request.url.includes("https://firebasestorage.googleapis.com")) {
+                return fetch(ev.request).then((response) => response);
+            }
+
+            return fetch(ev.request)
+                .then((fetchResponse) => {
+                    if (!fetchResponse) return fetchResponse;
+
+                    const type = fetchResponse.headers.get("content-type");
+
+                    if (
+                        type.match(/^text\/css/i) ||
+                        type.match(/^font\//i) ||
+                        type.match(/^image\/svg/i) ||
+                        type.match(/^application\/javascript/i)
+                    ) {
+                        return caches.open(cacheName).then((cache) => {
+                            return cache
+                                .put(ev.request, fetchResponse.clone())
+                                .then(() => fetchResponse);
+                        });
+                    }
+
+                    return fetchResponse;
+                })
+                .catch((error) => {
+                    return cacheResponse;
+                });
+        })
+    );
+});
