33869714f486db06d34d350694c396c6f246ed80
Dmytro Bazhanov <dimafg12@gmail.com>
2023-08-23 23:01:50 +0300

    SaleBadge - add caching to saleBadge
    SaleBadge - caching added
    SaleBadge - now retrieving information every time when application is loaded

--- src/components/saleBadge/SaleBadge.jsx
+++ src/components/saleBadge/SaleBadge.jsx
@@ -1,13 +1,44 @@
 import "./saleBadge.scss";
 import { getCurrentSale } from "../../queries/sales";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SaleBadge() {
     const [badgeSettings, setSettings] = useState({});
 
     const handleRefresh = async () => {
-        const settings = await getCurrentSale();
-        setSettings(settings);
+        const request = new Request("sales", { method: "GET" });
+
+        caches.open("v1").then((cache) => {
+            const handleRequest = () =>
+                getCurrentSale().then((saleObject) => {
+                    setSettings(saleObject);
+
+                    const now = new Date().getTime();
+                    const response = new Response(JSON.stringify({ saleObject, date: now }), {
+                        status: 200,
+                        statusText: "OK",
+                    });
+
+                    cache.put(request, response);
+                });
+
+            cache.match(request).then((cacheResponse) => {
+                if (cacheResponse) {
+                    const dayInMilliseconds = 86400000;
+                    cacheResponse.json().then((json) => {
+                        const now = new Date().getTime();
+                        if (now - dayInMilliseconds > Number(json.date)) {
+                            handleRequest();
+                        } else {
+                            setSettings(json.saleObject);
+                        }
+                    });
+                    return cacheResponse;
+                } else {
+                    handleRequest();
+                }
+            });
+        });
     };
 
     const style = {
@@ -20,10 +51,14 @@ export default function SaleBadge() {
         boxShadow: `0px 0px 3px 1px ${badgeSettings.color}`,
     };
 
+    useEffect(() => {
+        handleRefresh();
+    }, []);
+
     return (
         <div className="salesBadge" style={style} onClick={handleRefresh}>
             <span className="dot" style={dotStyle}></span>
-            {badgeSettings.name ?? "Click to refresh"}
+            {badgeSettings.name}
         </div>
     );
 }
