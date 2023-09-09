8a926c9b669e6645c718a0cb6ae1da2137640083
Dmytro Bazhanov <dimafg12@gmail.com>
2023-09-09 18:47:17 +0300

    Checkout - add responsiveness to checkout pages
    Checkout - responsiveness added to checkout checkoutForm
    Checkout - responsiveness added to total items display
    Checkout - total items display scroll styles edited

--- src/components/checkoutItems/CheckoutItems.jsx
+++ src/components/checkoutItems/CheckoutItems.jsx
@@ -8,9 +8,11 @@ export default function CheckoutItems({ items }) {
         <div className="checkout-items">
             <h1 className="checkout-items__header">Checkout Items</h1>
             <div className="checkout-items__item-container">
-                {items.map((item) => (
-                    <CheckoutItem key={item.instanceID} item={item} />
-                ))}
+                <div className="checkout-items__scroll-container">
+                    {items.map((item) => (
+                        <CheckoutItem key={item.instanceID} item={item} />
+                    ))}
+                </div>
             </div>
             <h1 className="checkout-items__total-price">
                 Total price: <span>${getTotalPrice(items)}</span>
